e9a6f8f8f751604dc5f5fb1cc6d1ad91a6143060
Luna Aphelion <luna-aphelion@proton.me>
2022-12-02 15:10:06 +0000

    added commands

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 
+const fs = require('node:fs');
+const path = require('node:path');
+
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
@@ -11,5 +14,41 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+client.commands = new Collection();
+
+// loads all command files
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+// command handler
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+	console.log(interaction);
+});
+
+
 
 client.login(process.env.BOT_TOKEN)
