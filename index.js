6317624587dcd30d69f3045907185ac82c470e77
Luna Aphelion <luna-aphelion@proton.me>
2022-12-02 14:19:21 +0000

    mend

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN)
